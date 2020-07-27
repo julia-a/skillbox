893de44d4eace160f065b22cdc4ed49216ec3f71
Julia Apreleva <12630m@mail.ru>
2020-07-27 14:17:08 +0300

    Module 1

--- module1/main.js
+++ module1/main.js
@@ -1,13 +1,13 @@
 'use strict';
-var date = new Date();
-var thisYear = date.getFullYear();
+const date = new Date();
+const thisYear = date.getFullYear();
 
-var greeting = function() {
-  var userName = prompt('Пожалуйста, напишите ваше имя') || 'Уважаемый';
-  var userSurname = prompt('Пожалуйста, напишите вашу фамилию?') || 'пользователь';
-  var userGreeting = userName + ' ' + userSurname + '!';
-  var userBirthYear = prompt('Введите ваш год рождения');
-  var age = thisYear - userBirthYear;
+const greeting = function() {
+  const userName = prompt('Пожалуйста, напишите ваше имя') || 'Уважаемый';
+  const userSurname = prompt('Пожалуйста, напишите вашу фамилию?') || 'пользователь';
+  const userGreeting = userName + ' ' + userSurname + '!';
+  const userBirthYear = prompt('Введите ваш год рождения');
+  const age = thisYear - userBirthYear;
 
   if (age < 20) {
     alert('Привет, ' + userGreeting);

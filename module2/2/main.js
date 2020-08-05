480cf94b7d57cd32efe2e7940ce9a1ffb82ebbcf
Julia Apreleva <12630m@mail.ru>
2020-08-05 22:25:43 +0300

    Доработки в Модуле-2 (задание 2,3)

--- module2/2/main.js
+++ module2/2/main.js
@@ -1,36 +1,45 @@
 'use strict';
+const isNum = function(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n); 
+};
+
+const isLeapYear = function() {
+  return ((i % 4 === 0) && (i % 100 !==0)) || (i % 400 === 0);
+};
+
 const showLeapYear = function() {
   let startYear = prompt('Вас приветствует интерфейс определения високосных годов. Пожалуйста, введите начальный год (или оставьте значение по-умолчанию: 1900 год).', 1900);
   if (startYear == null) {
-    return
+    return;
   };
-  
-  if (!isNaN(parseFloat(startYear)) && isFinite(startYear)) {
-  } else {
+
+  if (!isNum(startYear)) {
     alert('Ошибка! Значение начального года не является числом. Пожалуйста, перезагрузите страницу и повторите ввод.');
-    return
-  };
-   
+    return;
+  }
+  
   let endYear = prompt('Пожалуйста, введите конечный год (или оставьте значение по-умолчанию: 2000 год).', 2020);
   if (endYear == null) {
-    return
+    return;
   };
-  
-  if (!isNaN(parseFloat(endYear)) && isFinite(endYear)) {
-  } else {
+
+  if (!isNum(endYear)) {
     alert('Ошибка! Значение конечного года не является числом. Пожалуйста, перезагрузите страницу и повторите ввод.');
-    return
+    return;
   };
 
   if (startYear >= endYear) {
-    alert('Ошибка! Начальный год равен или больше конечного. Пожалуйста, перезагрузите страницу и повторите ввод.');
-    return
-  };  
-
-  for (let i = startYear; i <= endYear; i ++) {
-    if (((i % 4 === 0) && (i % 100 !==0)) || (i % 400 === 0)) {
-      console.log('year: ' + i);
-    }
-  }
+    for (let i = startYear; i > endYear; i --) {
+      if (isLeapYear) {
+        console.log('year: ' + i);
+      }
+    };
+  } else {
+    for (let i = startYear; i <= endYear; i ++) {
+      if (isLeapYear) {
+        console.log('year: ' + i);
+      }
+    };
+  };
 };
 showLeapYear();
--- module2/3/main.js
+++ module2/3/main.js
@@ -3,8 +3,14 @@ const sumNumbers = function() {
   let sum = 0;
   
   while (true) {
-    let messageValue = parseInt(prompt('Пожалуйста, введите число или нажмите «Отмена» чтобы вывести сумму ранее введенных чисел.'));
-    if (!messageValue) break;
+    let messageValue = prompt('Пожалуйста, введите число или нажмите «Отмена» чтобы вывести сумму ранее введенных чисел.');
+    if (messageValue == null) {
+      break;
+    } else if (messageValue == '') {
+      continue;
+    } else {
+      messageValue = parseInt(messageValue);
+    };
     sum += messageValue;
   }
   alert('Сумма: ' + sum)

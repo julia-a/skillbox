0e1296988d6364343e762708698c7e717853a112
Julia Apreleva <12630m@mail.ru>
2020-08-28 16:49:08 +0300

    Доработана функция getUserNumber

--- module3/2/main.js
+++ module3/2/main.js
@@ -1,19 +1,43 @@
 'use strict';
+const MAX_NUMBER = 10;
+
+const getRandomNumber = function(max) {
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max + 1));
+};
+
+const randomNumber = getRandomNumber(MAX_NUMBER);
+
 const isNum = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n); 
 };
 
+// Функция запрашивает ввод числа и передает его для сравнения с рандомным числом 
+// в следующую функцию numberСheck
 const getUserNumber = function() {
-  const messageValue = prompt('Пожалуйста, введи число или нажми «Отмена», чтобы прекратить работу программы.');
+  const inputNumber = prompt('Привет! Сыграем в игру? Я загадал число от 0 до 9. Попробуй отгадать его: введи в окошко ниже свой вариант и нажми «ОК».');
 
-  if (messageValue === null) {
-    return;
-  } else if (isNum(messageValue)) {
-    alert('Ура! Введено число: ' + messageValue);
-    return;
+  if (inputNumber === null) {
+    return alert('Пока! Как-нибудь сыграем ещё.');
+  } else if (isNum(inputNumber)) {
+    alert('Введено число: ' + inputNumber + '. Сравню его с загаданным.');
+    return +inputNumber;
   } else {
     alert('Что-то пошло не так: введено не число! Пожалуйста, попробуй еще раз.');
     getUserNumber();
   }
 };
-getUserNumber();
+
+// Функция, сверяющая введенное число с рандомным
+const numberСheck = function() {
+  const number = getUserNumber();
+
+  if (randomNumber === number) {
+    alert('Bingo! Ты отгадал!');
+  } else if (number > randomNumber) {
+    alert('Много.');
+  } else if (number < randomNumber) {
+    alert('Мало.');
+  }
+};
+numberСheck();
